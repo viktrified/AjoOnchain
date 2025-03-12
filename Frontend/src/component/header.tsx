a8e603eed6b4de3c44a094398f2131ab935c47f5
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2025-03-12 11:05:48 +0100

    very very sick

--- Frontend/src/component/header.tsx
+++ Frontend/src/component/header.tsx
@@ -2,8 +2,6 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Header = () => {
   const primaryColor = "#2f16d3"; // Slate blue
-  const backgroundColor = "#242424"; // Ash color
-  const textColor = "#1f1212";
 
   return (
     <div className="mb-8 text-center p-6 rounded-lg shadow flex justify-between items-center">
